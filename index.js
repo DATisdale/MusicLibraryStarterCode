600e416d81315673a2e998a149475b05c57a42dc
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-11-23 11:36:23 -0500

    committing first get request

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const cors =require('cors');
 const repoContext = require('./repository/repository-wrapper');
+
+
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(3000,function(){
     console.log("Server started. Listening on port 3000")
-app.get('/api/songs', (req,res)=>{
+
+    app.get('/api/songs', (req,res)=>{
     const songs= repoContext.songs.findAllSongs();
     return res.send(songs)
 })
+
 });
