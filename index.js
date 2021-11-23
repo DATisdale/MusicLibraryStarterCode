2a2a660b184e79e2d3624e984e57f4e5ac020eab
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-11-23 12:20:17 -0500

    fixed error internal server issue by correct syntax error

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(3000,function(){
     console.log("Server started. Listening on port 3000")
@@ -21,4 +22,15 @@ app.listen(3000,function(){
         const song= repoContext.songs.findSongById(id);
         return res.send(song);
     });
+    app.post('/api/songs', (req, res)=>{
+        const newSong = req.body;
+        const addedSong=repoContext.songs.createSong(newSong);
+        return res.send(addedSong)
+    })
+
+    app.delete('/api/songs/:id',(req, res)=>{
+        const id = req.params.id;
+        const updatedDataSet = repoContext.songs.deleteSong(id);
+        return res.send(updatedDataSet)
+    })
 });
